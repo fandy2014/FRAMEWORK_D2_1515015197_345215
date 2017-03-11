b815a01ebae6a453e488c874e1ccbe19f92dd08a
“Fandy” <fandyogep10@gmail.com>
2017-03-11 21:58:55 +0800

    Posttest

---
+++ database/migrations/2017_03_11_134925_buat_table_jadwal_matakuliah.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class BuatTableJadwalMatakuliah extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jadwal_matakuliah', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('jadwal_matakuliah');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title"><font color="red"><b>Fandy Tamvan</b></font></div>
             </div>
         </div>
     </body>
