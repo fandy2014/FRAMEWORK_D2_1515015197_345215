2eee7165c3d7ee9b73914a9e97f6cf0dee2b3f58
“Fandy” <fandyogep10@gmail.com>
2017-03-26 23:27:54 +0800

    Posttest4

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,6 +17,11 @@ Route::get('kelasd2/framework/{mhs?}',function($mhs="Anonim"){
 });
 Route::get('pengguna','PenggunaController@awal');
 Route::get('pengguna/tambah','PenggunaController@tambah');
+Route::get('pengguna/{pengguna}','PenggunaController@lihat');
+Route::post('pengguna/simpan','PenggunaController@simpan');
+Route::get('pengguna/edit/{pengguna}','PenggunaController@edit');
+Route::post('pengguna/{pengguna}','PenggunaController@update');
+Route::get('pengguna/hapus/{pengguna}','PenggunaController@hapus');
 
 Route::get('dosen','DosenController@awal');
 Route::get('dosen/tambah','DosenController@tambah');
@@ -32,6 +37,15 @@ Route::get('jadwal_matakuliah/tambah','Jadwal_matakuliahController@tambah');
 
 Route::get('matakuliah','MatakuliahController@awal');
 Route::get('matakuliah/tambah','MatakuliahController@tambah');
+Route::get('matakuliah/{matakuliah}','MatakuliahController@lihat');
+Route::post('matakuliah/simpan','MatakuliahController@simpan');
+Route::get('matakuliah/edit/{matakuliah}','MatakuliahController@edit');
+Route::post('matakuliah/edit/{matakuliah}','MatakuliahController@update');
+Route::get('matakuliah/hapus/{matakuliah}','MatakuliahController@hapus');
 
 Route::get('ruangan','RuanganController@awal');
 Route::get('ruangan/tambah','RuanganController@tambah');
+Route::post('ruangan/simpan','RuanganController@simpan');
+Route::get('ruangan/edit/{ruangan]','RuanganController@edit');
+Route::post('ruangan/edit/{ruangan}','RuanganController@update');
+Route::get('ruangan/hapus/{ruangan}','RuanganController@hapus');
